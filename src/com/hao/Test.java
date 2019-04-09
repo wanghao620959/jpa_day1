c3caf43c3a09d216e305beb4c2ed28b6060abfdd
sam <1440152867@qq.com>
2019-04-09 21:31:26 +0800

    第55次提交到

--- src/com/hao/Test.java
+++ src/com/hao/Test.java
@@ -7,10 +7,10 @@ public class Test {
         System.out.println("aaa");
         System.out.println("aaa");
 
-
         int[] arr={1,2,3};
         int[] ints = new int[5];
         System.out.println("5555");
+        System.out.println("6666");
 
     }
 }
